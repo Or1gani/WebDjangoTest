18299241bfd243f092d5cc0c4f1ca2dda1e61563
Or1gani <origani-chi@mail.ru>
2025-09-01 00:08:26 +0300

    Init project

--- webdjangotest/webdjangotest/settings.py
+++ webdjangotest/webdjangotest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-km&dq%-2b)iu3qxent_zp5^&jiu=*#*a-#vhr_53h3!5mh-0f='
+SECRET_KEY = 'чувак, ты думал здесь что-то будет? ООоо, нет. От тебя воняет, закрывай... закрывай иди от сюда'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
